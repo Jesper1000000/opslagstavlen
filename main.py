712d98995a2658ebe25164fe3c72079b77fe92aa
jesper1000000 <eaajhho@students.eaaa.dk>
2024-07-19 10:38:00 +0200

    added navbar

--- main.py
+++ main.py
@@ -11,7 +11,22 @@ app = Flask(__name__)
 
 def index():
     user = {'username': 'Jesper'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Jesper'},
+            'body': 'I love my dog!'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
     
     
