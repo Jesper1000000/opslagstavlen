f4fe0af4469866b745472827302c79d35af30c22
jesper1000000 <eaajhho@students.eaaa.dk>
2024-07-19 11:44:31 +0200

    added form

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create Flask's `app` object
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "super secret key"
 
+# Form class, with a single field for the name
+class NamerForum(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # Define a route for the default URL, which loads the form
 @app.route('/')
 @app.route('/index')
-
 def index():
     user = {'username': 'Jesper'}
     posts = [
@@ -28,14 +34,29 @@ def index():
 
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-    
-    
-
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
 
+# 
 @app.errorhandler(505)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForum()
+
+    # Validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template('name.html',
+                           name = name,
+                           form = form)
